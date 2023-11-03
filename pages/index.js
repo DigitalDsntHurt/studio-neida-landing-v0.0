fa753eaa8064223f999fdcaf9977a38def0c68e6
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2023-11-03 13:14:41 -0700

    init

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Studio Neida</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <a href="mailto:studio@studioneida.com" target="_blank">studio@studioneida.com</a>
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
